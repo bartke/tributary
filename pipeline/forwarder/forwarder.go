c25bdbeedfcc446abb817e840ed3b4596dd0d5ad
Andre Bartke <andre+git@bartke.cc>
2020-08-31 21:55:45 +0800

    add forwarder

---
+++ pipeline/forwarder/forwarder.go
@@ -0,0 +1,28 @@
+package forwarder
+
+import "github.com/bartke/tributary"
+
+type forwarder struct {
+	in  <-chan tributary.Event
+	out chan tributary.Event
+}
+
+func New() *forwarder {
+	return &forwarder{
+		out: make(chan tributary.Event),
+	}
+}
+
+func (f *forwarder) In(ch <-chan tributary.Event) {
+	f.in = ch
+}
+
+func (f *forwarder) Out() <-chan tributary.Event {
+	return f.out
+}
+
+func (f *forwarder) Run() {
+	for e := range f.in {
+		f.out <- e
+	}
+}
