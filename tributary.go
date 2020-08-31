435daf6ddc7611219640bc13dbbda13dc3d50826
Andre Bartke <andre+git@bartke.cc>
2020-08-31 21:57:00 +0800

    add fanin/fanout

--- tributary.go
+++ tributary.go
@@ -1,6 +1,8 @@
 package tributary
 
-import "context"
+import (
+	"context"
+)
 
 type Event interface {
 	Context() context.Context
@@ -28,4 +30,41 @@ type Pipeline interface {
 
 func Link(nodeA Source, nodeB Sink) {
 	nodeB.In(nodeA.Out())
+}
+
+func Fanin(nodeB Sink, nodes ...Source) {
+	out := make(chan Event)
+	nodeB.In(out)
+
+	for _, node := range nodes {
+		src := node.Out()
+		go func(ch <-chan Event) {
+			for e := range ch {
+				out <- e
+			}
+		}(src)
+	}
+}
+
+// Fanout links the source to all outputs
+func Fanout(nodeA Source, nodes ...Sink) {
+	in := nodeA.Out()
+	out := make([]chan Event, len(nodes))
+	for i := range nodes {
+		out[i] = make(chan Event)
+		nodes[i].In(out[i])
+	}
+	go func() {
+		for e := range in {
+			for i := range out {
+				// blocking forwarder, let's require an intact network
+				out[i] <- e
+				// debugging
+				//go func(ch chan Event) {
+				//	fmt.Println("sending", i, ch)
+				//	ch <- e
+				//}(out[i])
+			}
+		}
+	}()
 }
