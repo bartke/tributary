9917e1d885d16d7f077e9d6530354c68ff068b4a
Andre Bartke <andre+git@bartke.cc>
2020-09-05 13:40:51 +0800

    adapt scripted example

--- example/scripted/main.go
+++ example/scripted/main.go
@@ -6,22 +6,25 @@ import (
 
 	"github.com/bartke/tributary/example/common"
 	"github.com/bartke/tributary/module"
+	"github.com/bartke/tributary/network"
 	lua "github.com/yuin/gopher-lua"
 )
 
 func main() {
+	// create network and register nodes
+	n := network.New()
+	n.AddNode("ticker_1s", common.NewTicker())
+	n.AddNode("filter_even", common.NewFilter())
+	n.AddNode("printer", common.NewPrinter())
+
+	// create lua vm
 	vm := lua.NewState()
 	goCtx, ctxCancelFn := context.WithCancel(context.Background())
 	vm.SetContext(goCtx)
 	defer ctxCancelFn()
 
-	m := module.New()
-	// register network nodes
-	m.AddNode("ticker_1s", common.NewTicker())
-	m.AddNode("filter_even", common.NewFilter())
-	m.AddNode("printer", common.NewPrinter())
-
-	// register for lua 'require("tributary")
+	// create the tributary module and register the tributary module exports
+	m := module.New(n)
 	vm.PreloadModule("tributary", m.Loader)
 	network, err := compileLua("./example/scripted/network.lua")
 	if err != nil {
