3503fea0ef796caf05a2f4a67b8bb3b08f559cd1
Andre Bartke <andre+git@bartke.cc>
2020-09-07 08:57:34 +0800

    improve prev examples

--- example/scripted/network.lua
+++ example/scripted/network.lua
@@ -1,16 +1,18 @@
 local tb = require("tributary")
 
--- tb.link("ticker_1s", "filter_even")
--- tb.link("filter_even", "printer")
-
 tb.create_forwarder("fwd1")
-tb.fanout("ticker_1s", "filter_even", "fwd1")
-tb.fanin("printer", "filter_even", "fwd1")
+tb.create_forwarder("fwd2")
 
-print(tb.node_exists("fwd1"))
-print(tb.node_exists("fwd2"))
+tb.link("ticker_1s", "fwd1")
+tb.fanout("fwd1", "filter_even", "fwd2")
+tb.fanin("printer", "filter_even", "fwd2")
+
+print("fwd1 exists", tb.node_exists("fwd1"))
+print("fwd2 exists", tb.node_exists("fwd2"))
+print("fwd3 exists", tb.node_exists("fwd3"))
 
 tb.run("ticker_1s")
 tb.run("filter_even")
 tb.run("fwd1")
+tb.run("fwd2")
 tb.run("printer")
--- example/simple/main.go
+++ example/simple/main.go
@@ -8,25 +8,25 @@ import (
 
 func main() {
 	var source tributary.Source
-	var pipeline, fwd tributary.Pipeline
+	var pipeline, fwd1, fwd2 tributary.Pipeline
 	var sink tributary.Sink
 
 	source = common.NewTicker()
 	pipeline = common.NewFilter()
-	fwd = forwarder.New()
+	fwd1 = forwarder.New()
+	fwd2 = forwarder.New()
 	sink = common.NewPrinter()
 
-	//tributary.Link(source, pipeline)
-	//tributary.Link(pipeline, sink)
-
-	// from source to pipeline and forwarder
-	tributary.Fanout(source, pipeline, fwd)
+	// from source to pipeline and forwarder via fwd1
+	tributary.Link(source, fwd1)
+	tributary.Fanout(fwd1, pipeline, fwd2)
 	// to sink from pipeline and forwarder
-	tributary.Fanin(sink, pipeline, fwd)
+	tributary.Fanin(sink, pipeline, fwd2)
 
 	go source.Run()
 	go pipeline.Run()
-	go fwd.Run()
+	go fwd1.Run()
+	go fwd2.Run()
 	go sink.Run()
 
 	// blocking wait
