ece502935045837e689fe151e69126a3ebe9db9e
Andre Bartke <andre+git@bartke.cc>
2020-09-26 13:06:41 +0800

    add graphviz file (missing)

---
+++ graphviz.go
@@ -0,0 +1,86 @@
+package tributary
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	graphvizHeader = `digraph G {
+  rankdir=LR;
+  node [shape=box, colorscheme=pastel13];
+`
+	graphvizFooter = `}`
+)
+
+func sourceNodes(n Network) map[string]struct{} {
+	sources := map[string]struct{}{}
+	for src, _ := range n.Edges() {
+		hasDest := false
+		if n.NodeUnconnected(src) {
+			continue
+		}
+		for _, dests := range n.Edges() {
+			for _, dest := range dests {
+				if dest == src {
+					hasDest = true
+				}
+			}
+		}
+		if !hasDest {
+			sources[src] = struct{}{}
+		}
+	}
+	return sources
+}
+
+func drawGraphvizBootstrap(n Network) string {
+	var nodes string = "\n"
+	sources := sourceNodes(n)
+	var i int
+	for src, _ := range sources {
+		dest := "_" + strconv.Itoa(i)
+		nodes += fmt.Sprintf("  %s -> %s\n", src, dest)
+		nodes += fmt.Sprintf("  %s [shape=oval,fillcolor=2,style=radial];\n", src)
+		nodes += fmt.Sprintf("  %s [shape=oval,fillcolor=1,style=radial,label=_];\n", dest)
+		i++
+	}
+	for src, dests := range n.Edges() {
+		if !n.NodeUnconnected(src) {
+			continue
+		}
+		for j, dest := range dests {
+			src := "_" + strconv.Itoa(i+j)
+			nodes += fmt.Sprintf("  %s -> %s\n", src, dest)
+			nodes += fmt.Sprintf("  %s [shape=oval,fillcolor=2,style=radial,label=_];\n", src)
+			nodes += fmt.Sprintf("  %s [shape=oval,fillcolor=1,style=radial];\n", dest)
+			i++
+		}
+	}
+
+	return graphvizHeader + nodes + graphvizFooter
+}
+
+func drawGraphviz(n Network) string {
+	sources := sourceNodes(n)
+	var nodes string = "\n"
+	for src, dests := range n.Edges() {
+		for _, dest := range dests {
+			nodes += fmt.Sprintf("  %s -> %s\n", src, dest)
+			if _, is := sources[src]; is {
+				nodes += fmt.Sprintf("  %s [shape=oval,fillcolor=2,style=radial];\n", src)
+			}
+			if _, is := n.Edges()[dest]; !is {
+				nodes += fmt.Sprintf("  %s [shape=oval,fillcolor=1,style=radial];\n", dest)
+			}
+		}
+	}
+	return graphvizHeader + nodes + graphvizFooter
+}
+
+func Graphviz(n Network) string {
+	if n.IsConnected() {
+		return drawGraphviz(n)
+	}
+	return drawGraphvizBootstrap(n)
+}
